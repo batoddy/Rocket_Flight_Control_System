56b85ec7487797773cddb4ba01414d2327306210
Batuhan Odçıkın <116088684+batoddy@users.noreply.github.com>
2023-03-05 01:29:48 +0300

    Add files via upload

---
+++ TARS/Debug/Middlewares/Third_Party/FatFs/src/option/syscall.d
@@ -0,0 +1,6 @@
+Middlewares/Third_Party/FatFs/src/option/syscall.o: \
+ ../Middlewares/Third_Party/FatFs/src/option/syscall.c \
+ ../Middlewares/Third_Party/FatFs/src/option/../ff.h \
+ ../Middlewares/Third_Party/FatFs/src/option/../integer.h
+../Middlewares/Third_Party/FatFs/src/option/../ff.h:
+../Middlewares/Third_Party/FatFs/src/option/../integer.h:
